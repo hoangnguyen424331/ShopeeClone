8ec8ab89eeb591e8d0a17215e17768a6fc7ad229
Nguyen <hoangnguyen424331@gmail.com>
2022-12-17 13:39:59 +0700

    Feature: Setup router

---
+++ src/layouts/RegisterLayout/RegisterLayout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface Props {
+  children?: React.ReactNode
+}
+
+function RegisterLayout({ children }: Props) {
+  return <div>{children}</div>
+}
+
+export default RegisterLayout
