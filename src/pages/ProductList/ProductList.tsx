f81b3eeb783fdf3ed43e8f05f33450b726edbd80
Nguyen <hoangnguyen424331@gmail.com>
2022-12-21 12:06:08 +0700

    Feature: Implement Ui Component Product

--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import AsideFilter from './components/AsideFilter'
+import Product from './components/Product'
 import SortProductList from './components/SortProductList'
 
 export default function ProductList() {
@@ -12,7 +13,15 @@ export default function ProductList() {
           </div>
           <div className='col-span-9'>
             <SortProductList />
-            <div className='mt-6 grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'></div>
+            <div className='mt-6 grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
+              {Array(20)
+                .fill(0)
+                .map((_, index) => (
+                  <div key={index}>
+                    <Product />
+                  </div>
+                ))}
+            </div>
           </div>
         </div>
       </div>
