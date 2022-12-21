bc5aecb4b79edb189276a267fb9888be0b869f0b
Nguyen <hoangnguyen424331@gmail.com>
2022-12-21 14:23:59 +0700

    Feature: Integrate Api Product

--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -1,9 +1,22 @@
+import { useQuery } from '@tanstack/react-query'
 import React from 'react'
+import { productApi } from 'src/apis/product.api'
+import useQueryParams from 'src/hooks/useQueryParam'
+import { ProductListConfig } from 'src/types/product.types'
 import AsideFilter from './components/AsideFilter'
 import Product from './components/Product'
 import SortProductList from './components/SortProductList'
 
 export default function ProductList() {
+  const queryParams = useQueryParams()
+
+  const { data: productsData } = useQuery({
+    queryKey: ['products', queryParams],
+    queryFn: () => {
+      return productApi.getProducts(queryParams as ProductListConfig)
+    }
+  })
+
   return (
     <div className='bg-gray-200 py-6'>
       <div className='container'>
@@ -14,13 +27,11 @@ export default function ProductList() {
           <div className='col-span-9'>
             <SortProductList />
             <div className='mt-6 grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
-              {Array(20)
-                .fill(0)
-                .map((_, index) => (
-                  <div key={index}>
-                    <Product />
-                  </div>
-                ))}
+              {productsData?.data.data.products.map((product) => (
+                <div key={product._id}>
+                  <Product product={product} />
+                </div>
+              ))}
             </div>
           </div>
         </div>
--- src/pages/ProductList/components/Product/Product.tsx
+++ src/pages/ProductList/components/Product/Product.tsx
@@ -1,26 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Product as ProductType } from 'src/types/product.types'
+import { formatCurrency, formatNumberToSocialStyle } from 'src/types/utils.type'
 
-export default function Product() {
+interface Props {
+  product: ProductType
+}
+
+export default function Product({ product }: Props) {
   return (
     <Link to='/'>
       <div className='overflow-hidden rounded-sm bg-white shadow transition-transform duration-100 hover:translate-y-[-0.06rem] hover:shadow-md'>
         <div className='relative w-full pt-[100%]'>
           <img
-            src='https://cf.shopee.vn/file/sg-11134201-22120-a2ei76ihiwkv54_tn'
-            alt='product name'
+            src={product.image}
+            alt={product.name}
             className='absolute top-0 left-0 h-full w-full bg-white object-cover'
           />
         </div>
         <div className='overflow-hidden p-2'>
-          <div className='min-h-[32px] text-xs line-clamp-2'>
-            Áo Len Nam Cổ Lọ Lông Cừu Cao Cấp Dày Dặn Mềm Mịn Cực Ấm Kiểu Dáng Hàn Quốc Form Rộng Faviti
-          </div>
+          <div className='min-h-[32px] text-xs line-clamp-2'>{product.name}</div>
           <div className='mt-3 flex items-center'>
-            <div className='m-w-[50%] truncate text-gray-500 line-through'>₫200.000</div>
+            <div className='m-w-[50%] truncate text-gray-500 line-through'>
+              ₫{formatCurrency(product.price_before_discount)}
+            </div>
             <div className='ml-1 truncate text-orange'>
               <span className='text-xs'>₫</span>
-              <span>124.000</span>
+              <span>{formatCurrency(product.price)}</span>
             </div>
           </div>
           <div className='mt-3 flex items-center'>
@@ -63,7 +69,7 @@ export default function Product() {
                 ))}
             </div>
             <div className='ml-2 text-sm'>
-              <span>5.66k</span>
+              <span>{formatNumberToSocialStyle(product.sold)}</span>
               <span className='ml-1'>Đã bán</span>
             </div>
           </div>
